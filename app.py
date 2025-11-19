6527b73962ce389b90cb01d1df6f167552e04c28
Ibtissam EL <ibtissamel742@gmail.com>
2025-11-19 11:07:10 +0100

    basic todo app

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template, request, redirect, url_for
+
+app = Flask(__name__)
+
+tasks = [
+    {"id": 1, "title": "Acheter du cake", "done": False},
+    {"id": 2, "title": "Réviser Flask", "done": True},
+]
+
+@app.route("/")
+def home():
+    return render_template("index.html", tasks=tasks)
+
+@app.route("/add", methods=["POST"])
+def add_task():
+    title = request.form.get("title")
+
+    if title:
+        new_id = tasks[-1]["id"] + 1 if tasks else 1
+        tasks.append({"id": new_id, "title": title, "done": False})
+
+    return redirect(url_for("home"))  
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
