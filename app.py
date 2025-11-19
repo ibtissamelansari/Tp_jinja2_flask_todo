4047c30923a1084b3b6e1259b20e432a22316864
Ibtissam EL <ibtissamel742@gmail.com>
2025-11-19 11:43:04 +0100

    minor changes

--- app.py
+++ app.py
@@ -14,18 +14,16 @@ def home():
 @app.route("/add", methods=["POST"])
 def add_task():
     title = request.form.get("title")
-
     if title:
         new_id = tasks[-1]["id"] + 1 if tasks else 1
         tasks.append({"id": new_id, "title": title, "done": False})
+    return redirect(url_for("home"))
 
-    return redirect(url_for("home"))  
-
-@app.route("/done/<int:task_id>")
-def done_task(task_id):
+@app.route("/toggle/<int:task_id>")
+def toggle_task(task_id):
     for task in tasks:
         if task["id"] == task_id:
-            task["done"] = True
+            task["done"] = not task["done"]
             break
     return redirect(url_for("home"))
 
@@ -34,7 +32,6 @@ def delete_task(task_id):
     global tasks
     tasks = [task for task in tasks if task["id"] != task_id]
     return redirect(url_for("home"))
-
 
 if __name__ == "__main__":
     app.run(debug=True)
